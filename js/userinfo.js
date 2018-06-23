876901600650d07e246148a30ecb2664704d97c8
lvzhaohe <36439498+lvzhaohe@users.noreply.github.com>
2018-06-23 23:26:09 +0800

    dormitory
    select dormitory

---
+++ js/userinfo.js
@@ -0,0 +1,9 @@
+function transInfo(){
+var userphone=document.getElementById("userPhone");
+var userphone_show=document.getElementById("userphone_show");
+var userqq=document.getElementById("userqq");
+var userqq_show=document.getElementById("userqq_show");	
+	userphone_show.innerHTML=userphone.value;
+	usercity_show.innerHTML=place[0]+' '+place[1];
+	userqq_show.innerHTML=userqq.value;
+}
